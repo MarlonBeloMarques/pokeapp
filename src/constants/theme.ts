aff8b640728f74d937dcfd57b0e6c412adf21c59
MarlonBeloMarques <marlon.belohd@gmail.com>
2021-06-13 15:50:17 -0400

    theme constants

---
+++ src/constants/theme.ts
@@ -0,0 +1,52 @@
+const colors = {
+  primary: '#FFCB05',
+  secondary: '#3663AD',
+  white: '#FFFFFF',
+  black: '#000000',
+  apple: '#1C1C1C',
+  facebook: '#485A96',
+  google: '#FFFFFF',
+};
+
+const sizes = {
+  // global sizes
+  base: 16,
+  font: 14,
+  radius: 6,
+  padding: 25,
+
+  // font sizes
+  h1: 26,
+  h2: 20,
+  h3: 18,
+  title: 18,
+  header: 16,
+  body: 14,
+  caption: 12,
+};
+
+const fonts = {
+  h1: {
+    fontSize: sizes.h1,
+  },
+  h2: {
+    fontSize: sizes.h2,
+  },
+  h3: {
+    fontSize: sizes.h3,
+  },
+  header: {
+    fontSize: sizes.header,
+  },
+  title: {
+    fontSize: sizes.title,
+  },
+  body: {
+    fontSize: sizes.body,
+  },
+  caption: {
+    fontSize: sizes.caption,
+  },
+};
+
+export { colors, sizes, fonts };
