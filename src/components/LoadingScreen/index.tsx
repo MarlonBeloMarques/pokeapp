3d4938551d843f253becb50d59e4a439823cb8bf
MarlonBeloMarques <marlon.belohd@gmail.com>
2021-07-08 18:00:06 -0400

    update loading style

--- src/components/LoadingScreen/index.tsx
+++ src/components/LoadingScreen/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Modal } from 'react-native';
 import LottieView from 'lottie-react-native';
 import { Block } from '../../elements';
+import styles from './styles';
 
 interface Props {
   visible: boolean;
@@ -10,12 +11,14 @@ interface Props {
 const LoadingScreen: React.FC<Props> = ({ visible }) => (
   <Modal animationType="none" visible={visible}>
     <Block color="white" center middle>
-      <LottieView
-        source={require('../../assets/animations/loading.json')}
-        autoPlay
-        loop
-        style={{ width: 180, height: 180 }}
-      />
+      <Block style={styles.wrapperLoading}>
+        <LottieView
+          source={require('../../assets/animations/loading.json')}
+          autoPlay
+          loop
+          style={{ width: 180, height: 180 }}
+        />
+      </Block>
     </Block>
   </Modal>
 );
---
+++ src/components/LoadingScreen/styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+import { theme } from '../../constants';
+
+export default StyleSheet.create({
+  wrapperLoading: {
+    borderColor: theme.colors.primary,
+    borderBottomWidth: 3,
+  },
+});
