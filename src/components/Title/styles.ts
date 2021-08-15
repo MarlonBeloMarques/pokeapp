523c1ad610001aedf280a8d662d7fb1bce4d6fa1
Marlon Marques <marlon.belohd@gmail.com>
2021-08-15 10:41:29 -0400

    fix: title style android

--- src/components/Title/styles.ts
+++ src/components/Title/styles.ts
@@ -2,12 +2,21 @@ import { StyleSheet } from 'react-native';
 import { theme } from '../../constants';
 
 export default StyleSheet.create({
-  title: {
+  titleIOS: {
     color: theme.colors.primary,
     shadowColor: theme.colors.secondary,
     shadowOffset: { width: 2, height: 2 },
     shadowOpacity: 1,
     shadowRadius: 0.1,
     elevation: 3,
+  },
+  titleAndroidPrimary: {
+    color: theme.colors.primary,
+    position: 'absolute',
+    zIndex: 10,
+  },
+  titleAndroidSecondary: {
+    color: theme.colors.secondary,
+    left: -3,
   },
 });
